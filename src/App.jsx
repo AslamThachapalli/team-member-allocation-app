ded65de9275620393dc157505e0ca291fee574c2
AslamThachapalli <aslam.develop912@gmail.com>
2023-07-26 18:19:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Content from "./content";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
